448dd03cf952d9e2595e3b01fa59111a53eca870
InfinityOofs <38821230+zentag@users.noreply.github.com>
2021-06-18 21:04:20 +0000

    Added replit stuff, and also some error handling

--- commands/setup.js
+++ commands/setup.js
@@ -108,5 +108,27 @@ module.exports = {
     message.channel.send("Setup process finished! Thanks for adding us. ")
 
     })
-  }
+  },
+  error: ({ error, command, message, info }) => {
+    // "error" holds one of the strings mentioned in the above list
+    if (error === 'COMMAND DISABLED') {
+      // For example we can now create and send a custom embed
+      const embed = new Discord.MessageEmbed()
+        .setTitle('Command disabled')
+        .setColor(0xff0000)
+
+      message.reply(embed)
+    }
+    if (error === 'MISSING PERMISSIONS') {
+      // For example we can now create and send a custom embed
+      const embed = new Discord.MessageEmbed()
+        .setTitle('No Permissions!')
+        .setDescription("This command requires administrator, which you do not have! \nMy user commands: \n**stonks?activity**")
+        .setColor(0xff0000)
+      embed.attachFiles('./notstonks.jpg')
+      embed.setImage('attachment://notstonks.jpg');
+
+      message.reply(embed)
+    }
+  },
 }
--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const mongo = require("./mongo")
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+// Hopefully REPL Stuff
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+// End of REPL stuff
+
 botVersion = "1.0.0"
 mode = "normal"
 module.exports = botVersion, mode
@@ -12,7 +22,7 @@ module.exports = botVersion, mode
 client.on('ready', async () => {
     console.log('ready')
     mongoose.set('useFindAndModify', false);
-    client.user.setActivity("I HATE MY LIFE WHYYYYY", { type: "PLAYING"});
+    client.user.setActivity("S.A.M.! stonks?setup", { type: "PLAYING"});
     new WOKCommands(client, {
         commandsDir: 'commands',
         featureDir: 'features'
